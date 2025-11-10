4ecad37e19a231d557ffcad2e6ca7995bf92349c
snamisahoo <namitooha435@gmail.com>
2025-11-10 14:01:13 +0530

    Looping in JavaScript

---
+++ oct/week5/Day4/README.md
@@ -0,0 +1,29 @@
+LOOP IN JAVASCRIPT.
+=>loops are used to execute a piece of code again and again.
+
+1. for loop : 
+   
+   for(declaration and initialization ; condition ; updation){
+    
+    //statement
+
+   }
+EX :
+
+   for(let i=0(declaration and initialization); i<5(condition); i++(updation)){
+    console.log("hello")
+   }
+
+//it will print "hello" 5 times.
+
+   for(lt i=1;i<=5;i++){
+    console.log("hello")
+   }
+
+2. infinite loop : the terminating condition becomes true.
+
+3. while loop :
+   
+   while(condition){
+    //do some work or statement
+   }
---
+++ oct/week5/Day4/loop.js
@@ -0,0 +1,34 @@
+console.log("Today we will learn about looping")
+
+console.log("Qlith Ennovation")
+
+//for loop
+
+let i; 
+for(let i=0;i<=10;){
+    console.log("hello");
+
+    i++
+}
+
+console.log(i)
+
+
+//while loop 
+
+
+// let a=1;
+// while(a<=10){
+//     console.log("Qlith")
+// } the "Qlith" will print infinite times.
+
+// let a=1;
+// while(a<=10){
+//     console.log("Qlith")
+//     a++;
+// }
+// let arr=[1,2,3,4,5];
+// while(Arr.length>=0
+//     console.lawcdegtrgqtt4 gqq4 gr4ewt45afDATRrrrrrvtfWE2FWZ!q` Q!` 1~Q1`A  `q
+//     console.lawcdegtrgqtt4 gqq4 gr4ewt45afDATRrrrrrvtf
+// )
