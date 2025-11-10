47666e769ac9bff9399fc25f7c760be328e95a80
snamisahoo <namitooha435@gmail.com>
2025-11-10 13:36:36 +0530

    Operator in JavaScript

---
+++ oct/week5/Day2/README.md
@@ -0,0 +1,30 @@
+OPERATION IN JS:
+=>Used to to perform some operation on data.
+
+1. ARITHMATIC OPERATOR:
+
++, -, *, /
+
+I.modulus (%)
+II.exponentiation ()
+III.increment (++)
+IV.decrement (--)
+
+2. ASSIGNMENT OPERATOR:
+   
+   =, +=, -=, *=, %=, **=
+
+3. COMPARISION OPERATOR:
+   
+   <, >, <=, >=
+
+   I. equal t (=)
+   II.not equal to (!=)
+   III.equal and & type (==)
+   IV. not equal to & type (!==)
+
+4. LOGICAL OPERATOR:
+    
+    I. logical AND (&&)
+    II. logical OR (||)
+    III. logical NOT (!)
---
+++ oct/week5/Day2/oprrator.js
@@ -0,0 +1,90 @@
+console.log("today we are going to learn operator")
+
+let a=2;
+let b=3;
+// console.log(2+3)
+// console.log((a+2)+b)
+
+// var a;
+console.log(2+3)
+console.log(typeof (a+2))
+
+console.log(typeof (a+2))
+console.log(typeof typeof (a+2))
+console.log(a-2)
+console.log(a*2)
+console.log(a/2)
+console.log(a%2)
+console.log(a**2)
+
+//increment (post, pre)
+
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+
+
+//Decrement (post, pre)
+
+// console.log(a--)
+// console.log(a)
+// console.log(--a)
+
+
+// let c=a;
+// console.log(c)
+
+// let d=a;
+// d=d+2;
+// d+=2;
+// console.log(d)
+
+// let x=a;
+// x**=3;
+// console.log(x)
+
+//COMPARISION OPERATOR
+
+
+
+console.log(a<=b)
+console.log(a>b)
+
+// let num1=2;
+// let num2=2;
+// console.log(num1 == num2)
+
+// let num1=2;
+// let num2="2";
+// console.log(num1 == num2)
+
+// let num1=2;
+// let num2="2";
+// console.log(num1 === num2)
+
+// let num1=2;
+// let num2="test";
+// console.log(num1 == num2)
+
+// let num1=2;
+// let num2="3";
+// console.log(num1 == num2)
+
+// let num1=2;
+// let num2=2;
+// console.log(num1 != num2)
+
+// let num1=2;
+// let num2="1";
+// console.log(num1 != num2)
+
+let num1=2;
+let num2="1";
+// console.log(num1 !== num2)
+
+console.log((3>2) && (4>5))
+console.log((3>2) || (4>5))
+console.log(!true)
+console.log(!false)
+
+console.log((num2++ > 4)&& (num1 === num2) || (num1-- != num2))
