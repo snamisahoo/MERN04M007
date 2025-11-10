c4e14a0094404cb492ec7a2df46a4c188c2f17f9
snamisahoo <namitooha435@gmail.com>
2025-11-10 14:17:27 +0530

    String in JavaScript

---
+++ Nov/week1/Day1/string.js
@@ -0,0 +1,32 @@
+console.log("today we are going to study string")
+
+// let str="Hello";
+// console.log(str[0])
+
+// str[0]="s";
+// console.log(str[0]);
+
+//variable
+//object
+
+// console.log(str)
+
+// let strobj=new String("Bye");
+// console.log(strobj.valueOf()[2])
+
+// str=new String("New String");
+// console.log(str.length)
+
+//method and function
+
+//toUppercase()
+let str="new string";
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+let namestr="brand bacardi";
+console.log(namestr+"lemon")
+
+// console.log(namestr,30)
+// let ml=50;
+// console.log(brand bacardi in ${ml}ml)
